0543b0acc980f57eb509784964798306323fef26
Maxime Lenne <maxime.lenne@gmail.com>
2020-02-24 10:27:47 +0100

    add git provider + readme

--- aterminal.plugin.zsh
+++ aterminal.plugin.zsh
@@ -1,9 +1,28 @@
 # Docker info
+git_provider_promp_info() {
+  local REMOTE_ORIGIN_URL=`git config --get remote.origin.url`
+  if [[ ! -z `echo $REMOTE_ORIGIN_URL | grep 'github'` ]]
+  then
+    echo " \uf09b "
+  fi
+  if [[ ! -z `echo $REMOTE_ORIGIN_URL | grep 'gitlab'` ]]
+  then
+    echo " \uf296 "
+  fi
+  if [[ ! -z `echo $REMOTE_ORIGIN_URL | grep 'bitbucket'` ]]
+  then
+    echo " \uf171 "
+  fi
+}
+
+local git_provider_info='$(git_provider_promp_info)'
+
+# Docker info
 docker_prompt_info() {
   if which docker 2>/dev/null 1>/dev/null && [[ -f "./Dockerfile" ]] ; then
     DOCKER_VERSION=`docker -v | awk '{print substr($3, 0, length($3)-1)}'`
-    ZSH_THEME_DOCKER_PROMPT_PREFIX="%B%{%F{33}%}docker@"
-    ZSH_THEME_DOCKER_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_DOCKER_PROMPT_PREFIX="\uf308 "
+    ZSH_THEME_DOCKER_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_DOCKER_PROMPT_PREFIX$DOCKER_VERSION$ZSH_THEME_DOCKER_PROMPT_SUFFIX"
   fi
 }
@@ -14,8 +33,8 @@ local docker_info='$(docker_prompt_info)'
 npm_prompt_info() {
   if which npm 2>/dev/null 1>/dev/null && [[ -f "./package-lock.json" ]] ; then
     NPM_VERSION=`npm -v`
-    ZSH_THEME_NPM_PROMPT_PREFIX="%B%{%F{33}%}npm@"
-    ZSH_THEME_NPM_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_NPM_PROMPT_PREFIX="\ue71e "
+    ZSH_THEME_NPM_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_NPM_PROMPT_PREFIX$NPM_VERSION$ZSH_THEME_NPM_PROMPT_SUFFIX"
   fi
 }
@@ -26,8 +45,8 @@ local npm_info='$(npm_prompt_info)'
 yarn_prompt_info() {
  if which yarn 2>/dev/null 1>/dev/null && [[ -f "./yarn.lock" ]] ; then
    YARN_VERSION=`yarn -v`
-   ZSH_THEME_YARN_PROMPT_PREFIX="%B%{%F{33}%}yarn@"
-   ZSH_THEME_YARN_PROMPT_SUFFIX="%{$reset_color%} "
+   ZSH_THEME_YARN_PROMPT_PREFIX="yarn@"
+   ZSH_THEME_YARN_PROMPT_SUFFIX=" "
    echo "$ZSH_THEME_YARN_PROMPT_PREFIX$YARN_VERSION$ZSH_THEME_YARN_PROMPT_SUFFIX"
  fi
 }
@@ -38,8 +57,8 @@ local yarn_info='$(yarn_prompt_info)'
 node_prompt_info() {
   if which node 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep \.js$ | head -1` ]]; then
     NODE_VERSION=`node -v | awk '{print substr($1,2); }'`
-    ZSH_THEME_NODE_PROMPT_PREFIX="%B%{%F{33}%}node@"
-    ZSH_THEME_NODE_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_NODE_PROMPT_PREFIX="\uf898 "
+    ZSH_THEME_NODE_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_NODE_PROMPT_PREFIX$NODE_VERSION$ZSH_THEME_NODE_PROMPT_SUFFIX"
   fi
 }
@@ -50,8 +69,8 @@ local node_info='$(node_prompt_info)'
 ts_prompt_info() {
   if which tsc 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep '\.ts$' | head -1` || -f "./tsconfig.json" ]]; then
     TS_VERSION=`tsc -v | awk '{print substr($2, 0, length($2))}'`
-    ZSH_THEME_TS_PROMPT_PREFIX="%B%{%F{33}%}typescript@"
-    ZSH_THEME_TS_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_TS_PROMPT_PREFIX="\ufbe4 "
+    ZSH_THEME_TS_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_TS_PROMPT_PREFIX$TS_VERSION$ZSH_THEME_TS_PROMPT_SUFFIX"
   fi
 }
@@ -62,8 +81,8 @@ local ts_info='$(ts_prompt_info)'
 python_prompt_info() {
   if which python 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep \.py$ | head -1` ]]; then
     PYTHON_VERSION=`python -c 'import platform; print(platform.python_version())'`
-    ZSH_THEME_PYTHON_PROMPT_PREFIX="%B%{%F{33}%}python"
-    ZSH_THEME_PYTHON_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_PYTHON_PROMPT_PREFIX="python@"
+    ZSH_THEME_PYTHON_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_PYTHON_PROMPT_PREFIX$PYTHON_VERSION$ZSH_THEME_PYTHON_PROMPT_SUFFIX"
   fi
 }
@@ -74,8 +93,8 @@ local python_info='$(python_prompt_info)'
 go_prompt_info() {
   if which go 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep \.go$ | head -1` ]]; then
     GO_VERSION=`go version | awk '{print $3}'`
-    ZSH_THEME_GVM_PROMPT_PREFIX="%B%{%F{33}%}"
-    ZSH_THEME_GVM_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_GVM_PROMPT_PREFIX="go@"
+    ZSH_THEME_GVM_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_GVM_PROMPT_PREFIX$GO_VERSION$ZSH_THEME_GVM_PROMPT_SUFFIX"
   fi
 }
@@ -84,10 +103,10 @@ local go_info='$(go_prompt_info)'
 
 # Elixir info
 exl_prompt_info() {
-  if which elixir 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep \.ex$ | head -1` ]] || [[ ! -z `ls | grep \.exs$ | head -1` ]] ; then
+  if which elixir 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep '\.ex$' | head -1` ]] || [[ ! -z `ls | grep '\.exs$' | head -1` ]] ; then
     EXL_VERSION=`elixir -v | grep Elixir | awk '{print $2}'`
-    ZSH_THEME_EXL_PROMPT_PREFIX="%B%{%F{33}%}elixir"
-    ZSH_THEME_EXL_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_EXL_PROMPT_PREFIX="\ue62d "
+    ZSH_THEME_EXL_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_EXL_PROMPT_PREFIX$EXL_VERSION$ZSH_THEME_EXL_PROMPT_SUFFIX"
   fi
 }
@@ -98,8 +117,8 @@ local exl_info='$(exl_prompt_info)'
 ruby_prompt_info() {
   if which ruby 2>/dev/null 1>/dev/null && [[ ! -z `ls | grep \.rb$ | head -1` ]] || [[ -f 'Gemfile' || -f 'config.rb' || -f '.ruby-version' ]]; then
     RUBY_VERSION=`ruby -v | awk '{print $2}'`
-    ZSH_THEME_RUBY_PROMPT_PREFIX="%B%{%F{33}%}ruby"
-    ZSH_THEME_RUBY_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_RUBY_PROMPT_PREFIX="/e739 "
+    ZSH_THEME_RUBY_PROMPT_SUFFIX=" "
     echo "$ZSH_THEME_RUBY_PROMPT_PREFIX$RUBY_VERSION$ZSH_THEME_RUBY_PROMPT_SUFFIX"
   fi
 }
